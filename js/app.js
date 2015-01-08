4c530a8bac78cbdeb4d1cc7f64b2f53a19294c85
L McGuire <louise.h.mcguire@gmail.com>
2015-01-08 13:21:32 +0100

    Upload Final

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-  	$('.ryu').mouseenter(function() {
-  		$('.ryu-still').hide();
-  		$('.ryu-ready').show();
+
+    //"X" Keydown/Key Up Function - Cool
+    $('body').keydown(function(e) {
+      if (e.keyCode == 88 ) {
+        $('.ryu-action').hide();
+        $('.ryu-cool').show();
+      }
+      })
+
+      .keyup(function(e) {
+        if (e.keyCode == 88 ) {
+          $('.ryu-action').hide();
+          $('.ryu-still').show();  
+        }
+    });
+
+    //MouseEnter/MouseLeave - Ready
+    $('.ryu').mouseenter(function() {
+  		$('.ryu-action').hide();
+      $('.ryu-ready').show();
+      
   	})
 
   	.mouseleave(function() {
-    	$('.ryu-still').show();
-    	$('.ryu-ready').hide();
+    	$('.ryu-action').hide();
+      $('.ryu-still').show();
+    	
 })
+    //MouseDown/Mouseup - Throwing
   	.mousedown(function() {
   		playHadouken();
-  		$('.ryu-ready').hide();
+  		$('.ryu-action').hide();
   		$('.ryu-throwing').show();
   		$('.hadouken').finish().show()
   		.animate (
@@ -22,13 +42,12 @@ $(document).ready(function() {
   			}
   		);
   	})
-
   	.mouseup(function() {
-  		$('.ryu-throwing').hide();
-  		$('.hadouken').hide();
+  		$('.ryu-action').hide();
   		$('.ryu-ready').show();
   	});
 
+    //Throwing Sound Effects
   	function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
